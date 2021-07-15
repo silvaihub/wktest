8ea84be6ebc1a61a4b95d6486933d7ed87294b01
silvaihub <aghoghoza@gmail.com>
2021-07-15 12:01:09 +0100

    my test

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+let fruits = [ "apple","orange","mango"];
+console.log("for loop :")
+for(let i= 0; i <= fruits.length; i++)
+
+{
+    console.log(fruits[i])
+};
+console.log("for each loop :")
+fruits.forEach(function(a){
+    console.log(a)
+});
